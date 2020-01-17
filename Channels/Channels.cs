37c46894a016baa032e804c221d2ae185aa2337e
OmegaRogue <thatomegarogue@gmail.com>
2020-01-17 21:13:50 +0100

    Initial Commit

---
+++ Channels/Channels.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Nito.AsyncEx;
+
+namespace Channels
+{
+	public struct ChannelResult<T>
+	{
+		public T Val;
+		public bool Ok;
+	}
+	
+	public class Channel<T> : IEnumerable
+	{
+		private int _capacity;
+		private Queue<T> _contents;
+		private bool _closed;
+
+		public Channel(int capacity = 1)
+		{
+			_capacity = capacity;
+			_contents = new Queue<T>(capacity);
+		}
+		
+#pragma warning disable 1998
+		public async Task<ChannelResult<T>> PullAsync()
+
+		{
+			var output = new ChannelResult<T>{};
+			
+			bool received;
+			do
+			{
+				received = _contents.TryDequeue(out output.Val);
+				
+				if (_closed && !received)
+				{
+					output.Ok = false;
+					return output;
+				}
+			}
+			while (!received);
+			return output;
+		}
+
+		public ChannelResult<T> Pull()
+		{
+			var task = Task.Run(async () => await PullAsync());
+			return task.Result;
+		}
+
+		public void Push(T value) => Task.Run(async () => await PushAsync(value));
+
+		public async Task PushAsync(T value)
+		{
+			if(_closed)
+				return;
+			var done = _contents.Count < _capacity;
+			do
+			{
+				try
+				{
+					_contents.Enqueue(value);
+				}
+				catch
+				{
+					done = false;
+				}
+			}
+			while (!done);
+		}
+#pragma warning restore 1998
+
+		public void Close() => _closed = true;
+
+		public bool GetClosed() => _closed;
+		public IEnumerator<T> GetEnumerator()
+		{
+			var receiving = true;
+			while (receiving)
+			{
+				var result = Pull();
+				yield return result.Val;
+				receiving = result.Ok;
+			}
+		}
+ 
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+	}
+}
---
+++ ConcurrencyTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Channels;
+
+namespace ConcurrencyTest
+{
+	class Program
+	{
+		public static Channel<string> StringBuffer;
+		static void Main()
+		{
+			var startTime = DateTime.Now;
+			StringBuffer = new Channel<string>(100);
+			StringBuffer.Push("Start Time: " + startTime);
+			Work();
+			var secondTime = DateTime.Now;
+			var duration = secondTime - startTime;
+			StringBuffer.Push("Time After Starting Job: " + secondTime + " Duration: " + duration);
+
+			foreach (var received in StringBuffer)
+			{
+				Console.WriteLine(received);
+			}
+		}
+
+
+		static async Task Work()
+		{
+			var startTime = DateTime.Now;
+			await StringBuffer.PushAsync("Task Start Time: " + startTime);
+			await Task.Delay(5000);
+			var endTime = DateTime.Now;
+			var duration = endTime - startTime;
+			await StringBuffer.PushAsync("Task End Time: " + endTime +" Duration: " + duration);
+			StringBuffer.Close();
+		}
+	}
+}
